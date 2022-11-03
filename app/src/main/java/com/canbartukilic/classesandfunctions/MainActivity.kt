d840cbf072eec86ddfcab54c0808c9b074afd31e
Can Bartu Kılıç <91592857+cbartuk@users.noreply.github.com>
2022-11-03 11:10:10 +0300

    first commit

---
+++ app/src/main/java/com/canbartukilic/classesandfunctions/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.canbartukilic.classesandfunctions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        test()
+
+
+        // Void - Unit
+        mySum(10,20)
+
+        // Return
+
+        val result = myMultiply(10,20)
+        textView.text = "Result: ${result}"
+
+        /*
+        button.setOnClickListener {
+            println("clicked")
+        }
+        */
+
+
+
+        // Class
+        // Object & Instance
+        val homer = Simpsons("Homer Simpson",50,"Nuclear")
+        homer.hairColor = "Blonde"
+
+        //homer.age = 60
+        //homer.job = "Nuc"
+        //homer.name = "Homer"
+
+        val bart = Simpsons("Bart Simpson",13,"Student")
+        //bart.age = 13
+        //bart.name = "Bart Simpson"
+        //bart.job = "Student"
+
+        println(homer.hairColor)
+
+        // Nullability
+        // Nullable (?) && Non-null
+        // !!(kesin var) vs ?(bilmiyorum)
+
+        var myString : String? = null
+        var myAge : Int? = null
+
+        // 1) Null Safety
+
+        if (myAge != null) {
+            println(myAge * 10)
+        } else {
+            println("myAge null")
+        }
+
+        // 2) Safe Call
+
+        println(myAge?.compareTo(2))
+
+        // 3) Elvis Operator
+
+        val myResult = myAge?.compareTo(2) ?: -100
+        println(myResult)
+
+
+
+
+    }
+
+
+
+    fun test() {
+        println("test function")
+    }
+
+    // Input && Return
+    fun mySum(a: Int, b: Int) {
+        textView.text = "Result: ${a+b}"
+    }
+
+    fun myMultiply(x: Int, y:Int) : Int {
+        return x * y
+    }
+
+    fun helloKotlin(view : View) {
+        println("Hello Kotlin")
+    }
+}
---
+++ app/src/main/java/com/canbartukilic/classesandfunctions/Simpsons.kt
@@ -0,0 +1,23 @@
+package com.canbartukilic.classesandfunctions
+
+class Simpsons(var name:String, var age:Int, job:String) {
+
+    var hairColor = ""
+
+    fun changeHairColor() {
+        this.hairColor = "Yellow"
+    }
+    /*
+    // Property
+    var age = 0
+    var name = ""
+    var job = ""
+
+    constructor(ageInput: Int, nameInput: String, jobInput: String ) {
+        this.age = ageInput
+        this.name = nameInput
+        this.job = jobInput
+    }
+    */
+
+}
